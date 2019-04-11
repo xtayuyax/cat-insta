61e861efb19cca78a0f845bb77a5b31d36053fa7
unknown <jonathan.yulu@gmail.com>
2019-04-11 00:30:16 -0400

    ohoho

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import CatBreeds from "./components/cats/CatBreeds";
@@ -9,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <CatBreeds/>
         </header>
       </div>
--- src/components/cats/CatBreeds.jsx
+++ src/components/cats/CatBreeds.jsx
@@ -4,15 +4,45 @@ import axios from "axios";
 class CatBreeds extends React.Component{
     state = {
         breeds: [],
+        breed: "abys",
+        selectedBreed: {},
+        images: [],
     }
 
     componentDidMount(){
+        let queryParams = {
+            breed_ids: this.state.breed,
+            limit: 8
+        }
+
         axios.defaults.headers.common['x-api-key'] = "9905a9b7-0dbe-4977-b24f-5b960d83fa12"
 
         axios.get('https://api.thecatapi.com/v1/breeds/')
             .then(res => {
                 this.setState({breeds: res.data});
             }) 
+
+        axios.get('https://api.thecatapi.com/v1/images/search', { params: queryParams} )
+            .then(res => {
+                this.setState({images: res.data});
+            })
+    }
+
+    handleBreedChange = (event) => {
+        this.setState({breed: event.currentTarget.value});
+
+        let queryParams = {
+            breed_ids: event.currentTarget.value,
+            limit: 8
+        }
+
+        axios.get('https://api.thecatapi.com/v1/images/search/', { params: queryParams} )
+            .then(res => {
+                this.setState({images: res.data});
+            })
+
+        axios.get('https://api.thecatapi.com/v1/breeds/', {} ) 
+
     }
 
     renderBreedList(){
@@ -20,12 +50,55 @@ class CatBreeds extends React.Component{
 
         this.state.breeds.forEach(element => {
             breedList.push(
-                <option value = {element.name} key = {element.id}>{element.name}</option>
+                <option value = {element.id} key = {element.id}>{element.name}</option>
             )
         })
 
         return(
-            <select name = "breeds">{breedList}</select>
+            <select name = "breeds" value = {this.state.breed} onChange = {this.handleBreedChange}>{breedList}</select>
+        )
+    }
+
+    renderInfo(){
+        return(
+            <div className = "breedDesc">{this.state}</div>
+        )
+    }
+
+    renderImages(){
+        let x = 0;
+        let cloneImages1 = []
+        let cloneImages2 = []
+
+        if(this.state.images.length > 0){
+            this.state.images.forEach(image => {
+                if(x < 4){
+                    cloneImages1.push(
+                        <td>
+                            <img src = {image.url} alt = {this.state.breed + x} key = {x} />
+                        </td>
+                    )
+                }
+                else{
+                    cloneImages2.push(
+                        <td>
+                            <img src = {image.url} alt = {this.state.breed + x} key = {x}/>
+                        </td>
+                    )
+                }
+                x++;
+            })
+        }
+
+        return(
+            <table>
+                <tr>
+                    {cloneImages1}
+                </tr>
+                <tr>
+                    {cloneImages2}
+                </tr>
+            </table>
         )
     }
 
@@ -33,6 +106,7 @@ class CatBreeds extends React.Component{
         return(
             <div>
                 {this.renderBreedList()}
+                {this.renderImages()}
             </div>
         )
     }
